6c527e734ed7174f22d4c1a2a749ffd9fd796ef7
Akira Saito <akrstjp@users.sourceforge.jp>
2014-08-14 00:36:30 +0900

    add issue tree & history support

--- init.rb
+++ init.rb
@@ -6,6 +6,6 @@ require_dependency File.expand_path('../hooks', __FILE__)
 Redmine::Plugin.register :quick_view do
   name 'Quick View plugin'
   author 'Akira Saito'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  description 'This plugin provides ability to view a details of the issue at the issues page.'
+  version '0.0.2'
 end
