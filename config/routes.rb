924f54ae293765326b8e624b6c00b4f15c8a3b30
Akira Saito <akrstjp@users.sourceforge.jp>
2014-11-13 05:15:45 +0900

    add multiple viewing feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,12 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+RedmineApp::Application.routes.draw do
+
+  resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:index,:show]
+  resources :issues do
+    shallow do
+      #resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:index,:show]
+    end
+  end
+end
+
