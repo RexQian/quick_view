6a9eb309215d7a4241caca54e215adf8eba0f7e1
Akira Saito <akrstjp@users.sourceforge.jp>
2014-11-16 23:44:55 +0900

    cleanup

--- app/controllers/quick_view_issues_controller.rb
+++ app/controllers/quick_view_issues_controller.rb
@@ -1,14 +1,8 @@
 class QuickViewIssuesController < ApplicationController
-  #before_filter :find_project, :authorize, :only => [:show]
   before_filter :find_issue, :find_project_from_association, :only => [:show]
 
   def show
     respond_to do |format|
-      #logger.debug ' *** format='+format.inspect
-      #lookup_context.view_paths do |path|
-      #   logger.debug ' *** lookup='+path.to_s
-      #end
-      #logger.debug ' *** issue='+@issue.inspect
       format.html {
         render :template => 'quick_view_issues/quick_view_issue_dialog',
                :layout => false,
@@ -18,24 +12,10 @@ class QuickViewIssuesController < ApplicationController
   end
 
 private
-  def find_project
-    logger.info "project_id => #{params[:project_id]}"
-    @issue = Project.find(params[:project_id])
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
   def find_issue
     logger.info "issue_id => #{params[:id]}"
     @issue = @object = Issue.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-
-  def find_relation
-    @relation = IssueRelation.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render_404
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@
 # See: http://guides.rubyonrails.org/routing.html
 RedmineApp::Application.routes.draw do
 
-  resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:index,:show]
+  resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:show]
   resources :issues do
     shallow do
-      #resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:index,:show]
     end
   end
 end
