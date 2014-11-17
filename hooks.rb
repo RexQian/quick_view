13dd0b15e27c079aeef11f4f97ef76320b7e5e09
Akira Saito <akrstjp@users.sourceforge.jp>
2014-11-17 21:30:26 +0900

    cleanup

--- hooks.rb
+++ hooks.rb
@@ -2,6 +2,5 @@
 
 class QuickViewHooks < Redmine::Hook::ViewListener
 	render_on :view_issues_context_menu_end, :partial=> 'hooks/quick_view_context'
-        #render_on :view_issues_index_bottom, :partial=> 'hooks/quick_view_issues_footer'
         render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_view_issues_footer'
 end
