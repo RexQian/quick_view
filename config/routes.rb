ef93528d352ebbd4a72a9652ab033cc2685209bf
Akira Saito <akrstjp@users.sourceforge.jp>
2015-03-19 00:20:12 +0900

    implement(in progress): WorkTime plug-in extension. refs #1362 (http://www.r-labs.org/issues/1362)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 RedmineApp::Application.routes.draw do
 
   resources :quick_view_issues, :controller => 'quick_view_issues', :only => [:show]
+  resources :quick_view_worktime_issues, :controller => 'quick_view_worktime_issues', :only => [:show]
   resources :issues do
     shallow do
     end
--- hooks.rb
+++ hooks.rb
@@ -2,5 +2,6 @@
 
 class QuickViewHooks < Redmine::Hook::ViewListener
 	render_on :view_issues_context_menu_end, :partial=> 'hooks/quick_view_context'
+        render_on :view_layouts_base_html_head, :partial=> 'hooks/quick_view_issues_header'
         render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_view_issues_footer'
 end
