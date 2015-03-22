dc8e4dd8d50cb8a6cead4968bd2e40d3b16fda7c
Akira Saito <akrstjp@users.sourceforge.jp>
2015-03-23 01:51:51 +0900

    implement(in progress): WorkTime plug-in extension. refs #1362 (http://www.r-labs.org/issues/1362)

--- hooks.rb
+++ hooks.rb
@@ -2,6 +2,7 @@
 
 class QuickViewHooks < Redmine::Hook::ViewListener
 	render_on :view_issues_context_menu_end, :partial=> 'hooks/quick_view_context'
-        render_on :view_layouts_base_html_head, :partial=> 'hooks/quick_view_issues_header'
-        render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_view_issues_footer'
+  render_on :view_layouts_base_html_head, :partial=> 'hooks/quick_view_issues_header'
+  render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_view_issues_footer'
+  render_on :view_layouts_base_body_bottom, :partial=> 'hooks/quick_view_issues_footer_worktime_ext'
 end
