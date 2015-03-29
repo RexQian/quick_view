1f26f1c532c9ff686f8df77afb137594dfd06627
Akira Saito <akrstjp@users.sourceforge.jp>
2015-03-30 01:11:49 +0900

    add relations and subtask in the dialog.

--- app/controllers/quick_view_issues_controller.rb
+++ app/controllers/quick_view_issues_controller.rb
@@ -2,6 +2,8 @@ class QuickViewIssuesController < ApplicationController
   before_filter :find_issue, :find_project_from_association, :only => [:show, :portray]
 
   def show
+    @relations = @issue.relations.select {|r| r.other_issue(@issue) && r.other_issue(@issue).visible? }
+
     respond_to do |format|
       format.html {
         render :template => 'quick_view_issues/quick_view_issue_dialog',
