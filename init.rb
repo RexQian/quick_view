b9b428c76fca22c6d131ac7f5d81bfc605a1ce0b
Akira Saito <akrstjp@users.sourceforge.jp>
2015-03-31 23:10:40 +0900

    version 0.1.0

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :quick_view do
   name 'Quick View plugin'
   author 'Akira Saito'
   description 'This plugin provides ability to view a details of the issue at the issues page.'
-  version '0.0.6'
+  version '0.1.0'
 
   settings :default => {'issue_dialog_size'=>'640,600',
                         'issue_tooltip_enabled'=>'on',
